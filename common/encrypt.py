61ac94ffbe193b63ee2e9f968aace98384f8ae43
niuniuJQKKK <szm891018@163.com>
2022-04-25 23:20:52 +0800

    调整encrypt函数入参

--- common/encrypt.py
+++ common/encrypt.py
@@ -19,19 +19,19 @@ def _md5(txt: str):
     return hashlib.md5(txt.encode(encoding='UTF-8')).hexdigest()
 
 
-def encrypt(num: str, api: str):
+def encrypt(num: str, api: str, d_c0: str):
     """
     知乎加密
     :param num: headers中的x-zse-93
     :param api: 接口及后面的参数
+    :param d_c0: d_c0 必要参数
     :return: signature
     """
-    cookie = random.choice(COOKIE_LIST)
-    sign = _md5(f'{num}+{api}+{cookie}')
-    path = os.path.abspath(os.path.dirname(os.getcwd()))
-    with open(f'{path}/common/encrypt.js') as f:
-        js_signature = execjs.compile(f.read(), cwd=f'{path}/common/node_modules')
+    sign = _md5(f'{num}+{api}+{d_c0}')
+    path = os.path.dirname(__file__)
+    with open(f'{path}/encrypt.js') as f:
+        js_signature = execjs.compile(f.read(), cwd=f'{path}/node_modules')
         signature = js_signature.call('b', sign)
         signature = '2.0_' + signature
-    return signature, cookie
+    return signature
 
